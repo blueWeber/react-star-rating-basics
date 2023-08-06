46a68e567cf1cfa28cd6409c84ffede9b4862300
blueWeber <blueweber@gmail.com>
2023-08-06 17:12:05 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import StarRating from "./components/StarRating";
 
 function App() {
+  const [tempRate1, setTempRate1] = useState(3);
+  const [tempRate2, setTempRate2] = useState(2);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {tempRate1}
+      <br />
+      {tempRate2}
+      <StarRating
+        onSetRate={setTempRate1}
+        defaultRate={tempRate1}
+        className="aaa"
+      />
+      <StarRating max={10} onSetRate={setTempRate2} defaultRate={tempRate2} />
     </div>
   );
 }
